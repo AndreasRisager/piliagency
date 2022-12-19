1b3d9e2402b61b0764e4faa2aca75bdf624309d0
Andreas B. Risager <andreasbarkov@gmail.com>
2022-12-19 01:55:48 +0100

    default image for open graph

--- components/SEO.js
+++ components/SEO.js
@@ -1,12 +1,11 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import logo from "../public/logo.jpg";
 
 export default function SEO({
   title,
   description = "Digital Markedsføring der skaber performance! - Vi skaber succesfulde influencer marketing kampagner ved at forbinde influencers og brands",
   keywords = "influencer, influencer marketing, marketing, influency marketing agency, agency, influencer agency, digital markedsføring, markedsføring, brands, kampagner, influencer kampagner",
-  image,
+  image = "https://piliagency.com/logo.jpg",
 }) {
   const router = useRouter();
 
@@ -28,14 +27,14 @@ export default function SEO({
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title ? `${title} — Pili Agency` : "Pili Agency"} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image || logo} />
+      <meta property="og:image" content={image} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="https://piliagency.com/" />
       <meta property="twitter:url" content={`https://piliagency.com${router.pathname}`} />
       <meta name="twitter:title" content={title ? `${title} — Pili Agency` : "Pili Agency"} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image || logo} />
+      <meta name="twitter:image" content={image} />
     </Head>
   );
 }
